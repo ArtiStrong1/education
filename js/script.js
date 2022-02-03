d5abefa31008a008b8344ff900b7afe0464ceddf
artur <arti4049@gmail.com>
2022-02-03 18:07:15 +0200

    Do my first practice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+let numberOfFilms = prompt("Сколько фильмов вы посмотрели ?","");
+let personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    private: false,
+};
+const a = prompt("Последний фильм ?",""),
+      b = prompt("На сколько оцените ?",""),
+      c = prompt("Последний фильм ?",""),
+      d = prompt("На сколько оцените ?","");
+      personalMovieDB.movies[a] = b; 
+      personalMovieDB.movies[c] = d;
+      console.log(personalMovieDB);
