ec8a60efe7df20773e7e3279ed7234d0c1dac17d
artur <arti4049@gmail.com>
2022-02-07 11:18:12 +0200

    Do my third practice

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
          "use strict";
-         let numberOfFilms = prompt("Сколько фильмов вы посмотрели ?","");
+let numberOfFilms;
+         function start(){
+             numberOfFilms = prompt("Сколько фильмов вы посмотрели ?","");
+             while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
+                numberOfFilms = +prompt("Сколько фильмов вы посмотрели ?","");
+            }
+            
+        }
+// start();
          let personalMovieDB = {
              count:numberOfFilms,
              movies:{},
              actors:{},
              genres:[],
              private: false,
-         };
-         if (numberOfFilms < 10){
+        };
+function detectMyLevel(){ 
+        if (numberOfFilms < 10){
             alert("Просмотрено мало фильмов!");
         }else if (numberOfFilms >= 10 && numberOfFilms <= 30 ){
             alert('Вы классический зритель !');
@@ -16,24 +25,63 @@
         }else{
             alert("ОШИБКА");
         }
+        }
+// detectMyLevel();
 
-            //  for(let i = 0;i < 2; i++){
-            //     let a = prompt("Последний фильм ?",""),
-            //         b = prompt("На сколько оцените ?","");
-            //         if(a != null && b != null && a != "" && b != "" && a.length < 50){
-            //            personalMovieDB.movies[a] = b; 
-            //            console.log(personalMovieDB);
-            //         } else {console.log('ERROR');
-            //                 i--;
-            //             }
-            //  }
-             let it = 0;
-            do{
-                let a = prompt("Последний фильм ?",""),
-                    b = prompt("На сколько оцените ?","");
-                    it++;
+function rememberMyFilms() {
+    for(let i = 0;i < 2; i++){
+        let a = prompt("Последний фильм ?",""),
+            b = prompt("На сколько оцените ?","");
+                if(a != null && b != null && a != "" && b != "" && a.length < 50){
+                       personalMovieDB.movies[a] = b; 
+                       console.log(personalMovieDB);
+                    } else { console.log('ERROR');
+                            i--;
+                        }
+             }
+            }
+// rememberMyFilms();
 
-            }while(it <= 1);
+function writeYourGenres (){
+    for (let i = 1;i <= 3 ; i++){
+        let gen = prompt(`Какой ваш любимый ${i} жанр?`,'');  
+        personalMovieDB.genres[i-1] = gen;
+    }
+    
+} 
+// writeYourGenres ();
+
+function showMyDB (hidden){
+    if (!hidden) {
+        console.log (personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.private);
+
+        
+        
+        
+        
+        
+        
+        
+
+             
+
+
+
+
+            // function showFirstMessage (text){
+            //     console.log(text);
+            // }
+            // showFirstMessage("hello");
+
+
+            // function calc(a,b){
+            //     return a + b;
+            // }
+            // console.log(calc(5,4));
+
            
             
             
@@ -84,3 +132,21 @@
  //   console.log(num);
   //  num++;
 //}
+
+// let str = "Hello";
+// console.log(str.length);
+//  let arr =[1, 2, 4];
+//  console.log (arr.length);
+
+//  let fruit = "some fruit";
+//  console.log(fruit.indexOf("it"));
+
+//  const logg = "hello world" ;
+//  console.log (logg.slice(6,11));
+
+//  let test = 'Да сколько можно заниматься';
+//  console.log (test.slice(3,10));
+
+
+//  const num = 12.2;
+//  console.log (Math.round(num)); 
