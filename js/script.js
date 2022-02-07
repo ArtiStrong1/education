9ba394dc4bba7c18f2190136a6e497c90f0fefa8
artur <arti4049@gmail.com>
2022-02-07 11:54:28 +0200

    Callback function

--- js/script.js
+++ js/script.js
@@ -85,7 +85,23 @@ showMyDB(personalMovieDB.private);
 
            
             
+            function first (){
+                console.log ('1');
+            }
+            function second (){
+                console.log('2'); 
+            }
             
+            
+
+            function learnJS(lang, callback){
+               console.log (`Я учу :${lang}`);
+               callback();
+            }
+            function done(){
+                console.log('Я ПРОШЕЛ УРОК!');
+            }
+            learnJS ("JAVASCRIPT",done);
             
                
                
